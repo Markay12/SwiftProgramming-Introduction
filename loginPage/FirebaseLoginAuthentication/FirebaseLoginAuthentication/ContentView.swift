90daf0862349cf2a431ced0f098dac42fdd46270
Mark Ashinhust <mashinhu@asu.edu>
2023-03-16 16:50:20 -0700

    full setup without firebase included

--- loginPage/FirebaseLoginAuthentication/FirebaseLoginAuthentication/ContentView.swift
+++ loginPage/FirebaseLoginAuthentication/FirebaseLoginAuthentication/ContentView.swift
@@ -7,18 +7,7 @@
 
 import SwiftUI
 
-// Extension to use hex colors
-extension Color {
-    init(hex: UInt, alpha: Double = 1.0) {
-        self.init(
-            .sRGB,
-            red: Double((hex >> 16) & 0xff) / 255,
-            green: Double((hex >> 08) & 0xff) / 255,
-            blue: Double((hex >> 00) & 0xff) / 255,
-            opacity: alpha
-        )
-    }
-}
+
 
 
 struct ContentView: View {
@@ -50,23 +39,76 @@ struct ContentView: View {
             VStack(spacing: 20)
             {
                 
-                Text("Sign In")
+                Text("Sign Up")
                     .foregroundColor(.white)
                     .font(.system(size: 40, weight: .bold, design: .rounded))
                     .offset(x: -100, y: -150)
-                                
+                           
+                //email text field
                 TextField("Email", text: $email)
                     .foregroundColor(.white)
                     .textFieldStyle(.plain)
+                    .placeholder(when: email.isEmpty) {
+                        Text("Email")
+                            .foregroundColor(.white)
+                            .bold()
+                    }
                     
+                Rectangle()
+                    .frame(width: 350, height: 1)
+                    .foregroundColor(.white)
+                    
+                SecureField("Password", text: $password)
+                    .foregroundColor(.white)
+                    .textFieldStyle(.plain)
+                    .placeholder(when: password.isEmpty){
+                        Text("Password")
+                            .foregroundColor(.white)
+                            .bold()
+                    }
                 
                 Rectangle()
                     .frame(width: 350, height: 1)
                     .foregroundColor(.white)
                 
+                Button
+                {
+                    
+                    // sign in
+                    
+                } label: {
+                    Text("Sign Up")
+                        .bold()
+                        .frame(width: 200, height: 40)
+                        .background(
+                            RoundedRectangle(cornerRadius: 10, style: .continuous)
+                            .fill(.linearGradient(
+                                colors: [
+                                    Color(hex: 0xC1121F),
+                                    Color(hex: 0xf77f00),
+                                    Color(hex: 0xfcbf49)
+                                ],
+                                startPoint: .topLeading,
+                                endPoint: .bottomTrailing
+                            ))
+                            )
+                            .foregroundColor(.white)
+                }
+                .padding(.top)
+                .offset(y: 100)
                 
-                
-                
+                Button
+                {
+                    
+                    //login function
+                    
+                } label: {
+                    Text("Already Have an Account?")
+                        .bold()
+                        .foregroundColor(.white)
+                }
+                .padding(.top)
+                .offset(y: 110)
             }
             .frame(width: 350)
 
@@ -79,5 +121,32 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+
+extension View {
+    func placeholder<Content: View>(
+        when shouldShow: Bool,
+        alignment: Alignment = .leading,
+        @ViewBuilder placeholder: () -> Content) -> some View {
+
+        ZStack(alignment: alignment) {
+            placeholder().opacity(shouldShow ? 1 : 0)
+            self
+        }
+    }
+}
+
+// Extension to use hex colors
+extension Color {
+    init(hex: UInt, alpha: Double = 1.0) {
+        self.init(
+            .sRGB,
+            red: Double((hex >> 16) & 0xff) / 255,
+            green: Double((hex >> 08) & 0xff) / 255,
+            blue: Double((hex >> 00) & 0xff) / 255,
+            opacity: alpha
+        )
     }
 }
