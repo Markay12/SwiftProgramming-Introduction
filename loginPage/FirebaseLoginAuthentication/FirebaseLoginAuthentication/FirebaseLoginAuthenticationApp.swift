67d73d3fc36a673d530a11a44f735f7d001f5964
Mark Ashinhust <mashinhu@asu.edu>
2023-03-16 19:19:10 -0700

    firebase imported

--- loginPage/FirebaseLoginAuthentication/FirebaseLoginAuthentication/FirebaseLoginAuthenticationApp.swift
+++ loginPage/FirebaseLoginAuthentication/FirebaseLoginAuthentication/FirebaseLoginAuthenticationApp.swift
@@ -6,9 +6,18 @@
 //
 
 import SwiftUI
+import Firebase
 
 @main
 struct FirebaseLoginAuthenticationApp: App {
+    
+    init()
+    {
+        
+        FirebaseApp.configure()
+        
+    }
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
