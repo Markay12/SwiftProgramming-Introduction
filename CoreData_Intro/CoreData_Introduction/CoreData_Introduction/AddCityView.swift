af51375b3caa1b74f0df3bda0d1508aed7092d5d
Mark Ashinhust <mashinhu@asu.edu>
2023-03-31 17:29:37 -0700

    comments added to add city view

--- CoreData_Intro/CoreData_Introduction/CoreData_Introduction/AddCityView.swift
+++ CoreData_Intro/CoreData_Introduction/CoreData_Introduction/AddCityView.swift
@@ -10,12 +10,12 @@ import SwiftUI
 struct AddCityView: View {
     
     // Get the parameter information for the city
-    @State private var name = ""
-    @State private var description = ""
-    @State private var image = UIImage(named: "placeholder")!
-    @State private var showImagePicker = false
+    @State private var name = "" // city name
+    @State private var description = "" // city description
+    @State private var image = UIImage(named: "placeholder")! // city image
+    @State private var showImagePicker = false // flag to show/hide image picker
     
-    var onSave: (String, String, UIImage) -> Void
+    var onSave: (String, String, UIImage) -> Void // closure to save city info
     
     // Setup body of the sheet.
     var body: some View {
@@ -24,15 +24,15 @@ struct AddCityView: View {
             // Form for the user to add the information
             Form {
                 Section(header: Text("City Info")) {
-                    TextField("Name", text: $name)
-                    TextField("Description", text: $description)
+                    TextField("Name", text: $name) // text field to enter city name
+                    TextField("Description", text: $description) // text field to enter city description
                     Button(action: {
-                        self.showImagePicker = true
+                        self.showImagePicker = true // show image picker when button is tapped
                     }, label: {
-                        Text("Select Image")
+                        Text("Select Image") // button to select image
                     })
                     
-                    Image(uiImage: image)
+                    Image(uiImage: image) // display selected image
                         .resizable()
                         .scaledToFit()
 
@@ -40,14 +40,14 @@ struct AddCityView: View {
                 }
                 Button(action: {
                     
-                    onSave(name, description, image)
+                    onSave(name, description, image) // call onSave closure to save city info
                 }, label: {
-                    Text("Save")
+                    Text("Save") // button to save city info
                 })
             }
-            .navigationTitle("Add City")
+            .navigationTitle("Add City") // set title of navigation bar
             .sheet(isPresented: $showImagePicker) {
-                ImagePicker(sourceType: .photoLibrary, selectedImage: $image)
+                ImagePicker(sourceType: .photoLibrary, selectedImage: $image) // show image picker as a sheet
             }
             
         }
