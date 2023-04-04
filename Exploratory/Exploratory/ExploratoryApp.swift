b90583dfa76ba47f0a357f5db6174ccd8b7c791b
Markay <54555076+Markay12@users.noreply.github.com>
2023-04-04 13:59:35 -0700

    New Project Init

---
+++ Exploratory/Exploratory/ExploratoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExploratoryApp.swift
+//  Exploratory
+//
+//  Created by Mark Ashinhust on 4/4/23.
+//
+
+import SwiftUI
+
+@main
+struct ExploratoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
