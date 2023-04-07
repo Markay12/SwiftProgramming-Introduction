7cebe2665e9152da3e4fb2d3f294c843187e11c6
Markay <54555076+Markay12@users.noreply.github.com>
2023-04-07 16:17:05 -0700

    Updated comments for working JSON data

--- JSONDataAccess/JSONDataAccess/ContentView.swift
+++ JSONDataAccess/JSONDataAccess/ContentView.swift
@@ -8,26 +8,36 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    //City string access
     @State private var city: String = ""
     @State private var showSheet = false
     
     var body: some View {
-        Form {
-            Section {
-                TextField("Enter your city name or address", text: $city)
-            }
-            
-            Section {
-                Button("Search") {
-                    showSheet = true
+        NavigationView {
+            Form {
+                Section {
+                    
+                    Text("Earthquake Information by City")
+                        .bold()
+                    
+                    // Enter the city we want to search for
+                    TextField("Enter your city name or address", text: $city)
                 }
-                .sheet(isPresented: $showSheet)
-                {
-                    EarthquakeDataView(city: city)
+                
+                Section {
+                    // Button to search.
+                    Button("Search") {
+                        showSheet = true
+                    }
+                    //Show the sheet that includes the Earthquake information
+                    .sheet(isPresented: $showSheet)
+                    {
+                        EarthquakeDataView(city: city)
+                    }
                 }
             }
         }
-        .navigationBarTitle("Earthquake Information")
     }
 }
 
--- JSONDataAccess/JSONDataAccess/EarthquakeDataView.swift
+++ JSONDataAccess/JSONDataAccess/EarthquakeDataView.swift
@@ -8,24 +8,34 @@
 import SwiftUI
 import MapKit
 
+// Structure to hold the datetime and the magnitude for Earthquake information
 struct Earthquake: Codable {
     let datetime: String
     let magnitude: Double
 }
 
 struct EarthquakeDataView: View {
+    
+    // Earthquake data variable
     @State private var earthquakes = [Earthquake]()
+    
+    // Input city string coming from the user
     let city: String
     
+    // Latitude and longitude values
     @State private var latitude: CLLocationDegrees = 0
     @State private var longitude: CLLocationDegrees = 0
     
+    // Main view
     var body: some View {
         VStack {
+            
+            // Update title to reflect the city and/or address provided
             Text("Earthquake Data for \(city)")
-                .font(.subheadline)
+                .font(.headline)
                 .padding()
             
+            // List out the earthquake data, only 10 using the prefix
             List(earthquakes.prefix(10), id: \.datetime) { earthquake in
                 VStack(alignment: .leading) {
                     Text("Date: \(earthquake.datetime)")
@@ -37,25 +47,30 @@ struct EarthquakeDataView: View {
             }
             .onAppear {
                 getLocation(for: city)
-                
-                
+     
             }
         }
     }
     
+    // Function to get the location based off the city string that was given.
+    // This should work for both addresses and just city names.
     func getLocation(for city: String) {
         let geocoder = CLGeocoder()
         geocoder.geocodeAddressString(city) { placemarks, error in
             guard let placemark = placemarks?.first, error == nil else {
+                // error if the location cannot be found
                 print("Error: \(error?.localizedDescription ?? "Unknown error")")
                 return
             }
+            // get the latitude and longitude based on the given placemark
             self.latitude = placemark.location?.coordinate.latitude ?? 0
             self.longitude = placemark.location?.coordinate.longitude ?? 0
             self.loadData()
         }
     }
     
+    // load data for the CLGeocoder to retreive city location and information.
+    // This is what allows us to get the location for a city by name.
     func loadData() {
         CLGeocoder().geocodeAddressString(city) { placemarks, error in
             guard let placemark = placemarks?.first, let location = placemark.location else {
@@ -66,20 +81,25 @@ struct EarthquakeDataView: View {
             let latitude = location.coordinate.latitude
             let longitude = location.coordinate.longitude
             
+            // Using what was given in the class structure, we can +- 10 to get the values for N, E, S, W
             let north = latitude + 10
             let south = latitude - 10
             let east = longitude + 10
             let west = longitude - 10
             
+            // JSON URL that uses the north, east, south and west data for its location access
             let urlString = "http://api.geonames.org/earthquakesJSON?north=\(north)&south=\(south)&east=\(east)&west=\(west)&username=markay"
             
+            // Checking in case of an invalid given URL
             guard let url = URL(string: urlString) else {
                 print("Invalid URL")
                 return
             }
             
+            // URL Request
             let request = URLRequest(url: url)
             
+            // invoke the URL Request to get the location and earthquake data
             URLSession.shared.dataTask(with: request) { data, response, error in
                 if let data = data {
                     if let decodedResponse = try? JSONDecoder().decode([String: [Earthquake]].self, from: data),
@@ -90,13 +110,14 @@ struct EarthquakeDataView: View {
                         return
                     }
                 }
+                // In case of a missing city or bad given data
                 print("Fetch failed: \(error?.localizedDescription ?? "Unknown error")")
             }.resume()
         }
     }
 }
 
-
+// Quick view for the Canvas
 struct EarthquakeDataView_Previews: PreviewProvider {
     static var previews: some View
     {
