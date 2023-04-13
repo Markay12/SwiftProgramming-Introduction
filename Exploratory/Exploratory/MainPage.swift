a09dfef29be1e24cb0d3bcd9c7652fec3b963149
Markay <54555076+Markay12@users.noreply.github.com>
2023-04-13 10:48:28 -0700

    init MapView

--- Exploratory/Exploratory/MainPage.swift
+++ Exploratory/Exploratory/MainPage.swift
@@ -6,10 +6,25 @@
 //
 
 import SwiftUI
+import MapKit
 
 struct MainPage: View {
+    
+    @StateObject private var viewModel = MapViewModel()
+    
+    // Beginning location data at Arizona State University Tempe
+    // Span of 0.005 to be closer to the users location
+    @State private var region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 33.42348546947854, longitude: -111.9314781482712),
+        span: MKCoordinateSpan(latitudeDelta: 0.005, longitudeDelta: 0.005))
+    
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        // Show the map
+        // Show user location if permission is given
+        Map(coordinateRegion: $region, showsUserLocation: true)
+            .ignoresSafeArea() // Remove the borders
+        
     }
 }
 
@@ -17,4 +32,63 @@ struct MainPage_Previews: PreviewProvider {
     static var previews: some View {
         MainPage()
     }
+}
+
+final class MapViewModel: NSObject, ObservableObject, CLLocationManagerDelegate {
+    
+    // Need location manager to use maps
+    // Optional value in case the user has their location services turned off
+    var locationManager:  CLLocationManager?
+    
+    // Check to make sure the location services are enables
+    func checkIfLocationServicesEnabled() {
+        if CLLocationManager.locationServicesEnabled()
+        {
+            
+            // True, so create the location manager
+            locationManager = CLLocationManager()
+            
+            // Desired user location accuracy
+            locationManager?.desiredAccuracy = kCLLocationAccuracyBest
+            
+        }
+        else
+        {
+            
+            print("Alert that the location is turned off and they should turn it on.")
+            
+        }
+    }
+    
+    // Get authorization for location from the user
+    func checkLocationAuthorization()
+    {
+        
+        guard let locationManager = locationManager else { return }
+        
+        // Check for all auth cases
+        switch locationManager.authorizationStatus {
+            
+            case .notDetermined:
+                locationManager.requestWhenInUseAuthorization()
+            case .restricted:
+                print("Show alert about location")
+            case .denied:
+                print("This app cannot access your location. Please allow location data.")
+            case .authorizedAlways, .authorizedWhenInUse:
+                break
+            @unknown default:
+                break
+        }
+        
+        
+    }
+    
+    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+        
+        // If location management has changed we want to check the authorization again
+        checkLocationAuthorization()
+    }
+    
+    
 }
