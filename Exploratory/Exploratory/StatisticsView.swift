c6669e1aa39a3fb530ad137d3f743f302388e1ce
Markay <54555076+Markay12@users.noreply.github.com>
2023-04-13 19:30:52 -0700

    Updated sheet data

--- Exploratory/Exploratory/StatisticsView.swift
+++ Exploratory/Exploratory/StatisticsView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct StatisticsView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text("This is the user statistics sheet.")
     }
 }
 
