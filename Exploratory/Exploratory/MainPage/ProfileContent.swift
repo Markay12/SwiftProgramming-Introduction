c295ace683cf5d5048085c6127e7b562b0f1fc03
Markay <54555076+Markay12@users.noreply.github.com>
2023-04-16 12:06:09 -0700

    Setup settings page for user

---
+++ Exploratory/Exploratory/MainPage/ProfileContent.swift
@@ -0,0 +1,17 @@
+//
+//  ProfileContent.swift
+//  Exploratory
+//
+//  Created by Mark Ashinhust on 4/16/23.
+//
+
+import Foundation
+import SwiftUI
+import SDWebImageSwiftUI
+
+struct ProfileContent: View {
+    var body: some View {
+        Text("Hello, world!")
+    }
+    
+}
---
+++ Exploratory/Exploratory/MainPage/SettingsView.swift
@@ -0,0 +1,155 @@
+//
+//  SettingsView.swift
+//  Exploratory
+//
+//  Created by Mark Ashinhust on 4/13/23.
+//
+
+import SwiftUI
+import FirebaseAuth
+import Firebase
+import FirebaseStorage
+
+struct SettingsView: View {
+    
+    // MARK: Setup variables for settings
+    @State private var myProfile: User?
+    
+    // App storage data
+    @AppStorage("log_status") var logStatus: Bool = false
+    
+    // Loading wheel
+    @State private var isLoading: Bool = false
+    
+    @State var errorMessage: String = ""
+    @State var showError: Bool = false
+    
+    var body: some View {
+                
+        NavigationStack
+        {
+            VStack
+            {
+                if let myProfile
+                {
+                    ProfileContent(user: myProfile)
+                }
+                else
+                {
+                    MainPage()
+                }
+            }
+            // Make the user data able to be refreshed
+            .refreshable {
+                // MARK: Refresh the user Data
+                myProfile = nil //set user data to nil, then refresh
+                await fetchUserData()
+            }
+            .navigationTitle("My Profile")
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing)
+                {
+                    Menu
+                    {
+                        
+                        // MARK: Logout / Delete Account
+                        Button("Logout", action: logout)
+                        
+                        Button("Delete Account", role: .destructive, action: deleteAccount)
+                    
+                    } label: {
+                        Image(systemName: "ellipsis")
+                            .rotationEffect(.init(degrees: 90))
+                            .tint(.black)
+                            .scaleEffect(0.8)
+                    }
+                }
+            }
+        }
+        .overlay{
+            LoadingView(show: $isLoading)
+        }
+        .alert(errorMessage, isPresented: $showError)
+        {
+            
+        }
+        .task {
+            
+            // Detched user data on the first time only
+            if myProfile != nil {return}
+            
+            // Initial Fetch user data
+            await fetchUserData()
+        }
+    }
+    
+    // MARK: Fetch the Users data
+    func fetchUserData() async
+    {
+        
+        guard let userUID = Auth.auth().currentUser?.uid else {return}
+        guard let user = try? await Firestore.firestore().collection("Users").document(userUID).getDocument(as: User.self) else {return}
+        await MainActor.run(body: {
+            myProfile = user
+        })
+        
+    }
+    
+    // MARK: Logout Function
+    func logout()
+    {
+        try? Auth.auth().signOut()
+        
+        print("User Logged Out")
+        logStatus = false
+        
+        print(logStatus)
+
+    }
+    
+    // MARK: Delete Account
+    
+    func deleteAccount()
+    {
+        isLoading = true
+        Task
+        {
+            do {
+                guard let userUID = Auth.auth().currentUser?.uid else{return}
+                
+                // Delete profile image from storage
+                let reference = Storage.storage().reference().child("Profile_Images").child(userUID)
+                try await reference.delete()
+                
+                // Delete Firestore user doc
+                try await Firestore.firestore().collection("Users").document(userUID).delete()
+                
+                // Delete auth account and set logStatus to false
+                try await Auth.auth().currentUser?.delete()
+                logStatus = false
+                
+            }
+            catch {
+                await setError(error)
+                isLoading = false
+            }
+        }
+        
+    }
+    
+    // MARK: Error message
+    func setError(_ error: Error) async {
+        // UI Must be run on the main thread
+        await MainActor.run(body: {
+            errorMessage = error.localizedDescription
+            showError.toggle()
+        })
+    }
+    
+}
+
+struct SettingsView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
