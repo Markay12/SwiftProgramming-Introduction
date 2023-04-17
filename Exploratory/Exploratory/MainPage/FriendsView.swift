2f3ea862168a7295dc6b4966af569f4622c91bd2
Markay <54555076+Markay12@users.noreply.github.com>
2023-04-17 12:19:10 -0700

    Search for user implemented

--- Exploratory/Exploratory/MainPage/FriendsView.swift
+++ Exploratory/Exploratory/MainPage/FriendsView.swift
@@ -6,10 +6,75 @@
 //
 
 import SwiftUI
+import FirebaseFirestore
 
 struct FriendsView: View {
+    
+    // MARK: Add Variables
+    @State private var fetchedUsers: [User] = []
+    @State private var searchText: String = ""
+    // Dismiss environment
+    @Environment(\.dismiss) private var dismiss
+    
     var body: some View {
-        Text("Welcome to the friends sheet.")
+     
+        NavigationStack
+        {
+            //List for the users
+            List {
+                ForEach(fetchedUsers) { user in
+                    NavigationLink {
+                        
+                    } label: {
+                        Text(user.username)
+                            .font(.callout)
+                            .hAlign(.leading)
+                    }
+                }
+                
+            }
+            .listStyle(.plain)
+            // Navigation title and information
+            .navigationBarTitleDisplayMode(.inline)
+            .navigationTitle("Search for a User")
+            .searchable(text: $searchText)
+            .onSubmit(of: .search, {
+                // MARK: Fetch user from Firebase
+                Task {
+                    await searchUsers()
+                }
+            })
+            .onChange(of: searchText, perform: { newValue in
+                if newValue.isEmpty {
+                    fetchedUsers = []
+                }
+            })
+            
+        }
+        
+    }
+    func searchUsers() async
+    {
+        do {
+            
+            let documents = try await Firestore.firestore().collection("Users")
+                .whereField("username", isGreaterThanOrEqualTo: searchText)
+                .whereField("username", isLessThanOrEqualTo: "\(searchText)\u{f8ff}")
+                .getDocuments()
+            
+            // User information
+            let users = try documents.documents.compactMap { doc -> User? in
+                try doc.data(as: User.self)
+            }
+            
+            // on Main thread
+            await MainActor.run(body: {
+                fetchedUsers = users
+            })
+        }
+        catch {
+            print(error.localizedDescription)
+        }
     }
 }
 
