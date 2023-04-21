97a1a715d3a7912f043ab0d25a69cfdcb4fe5ea5
Markay <54555076+Markay12@users.noreply.github.com>
2023-04-20 18:49:02 -0700

    Basic city history added

--- Exploratory/Exploratory/MainPage/HistoryView.swift
+++ Exploratory/Exploratory/MainPage/HistoryView.swift
@@ -8,8 +8,46 @@
 import SwiftUI
 
 struct HistoryView: View {
+    let cities = [
+        ["name": "New York", "description": "The city that never sleeps."],
+        ["name": "Los Angeles", "description": "The City of Angels."],
+        ["name": "Chicago", "description": "The Windy City."],
+        ["name": "Houston", "description": "Space City."],
+        ["name": "Phoenix", "description": "The Valley of the Sun."],
+        ["name": "Philadelphia", "description": "The City of Brotherly Love."],
+        ["name": "San Antonio", "description": "The Alamo City."],
+        ["name": "San Diego", "description": "America's Finest City."],
+        ["name": "Dallas", "description": "The Big D."],
+        ["name": "San Francisco", "description": "The Golden Gate City."],
+        ["name": "Austin", "description": "The Live Music Capital of the World."],
+        ["name": "Seattle", "description": "The Emerald City."],
+        ["name": "Denver", "description": "The Mile High City."],
+        ["name": "Miami", "description": "The Magic City."],
+        ["name": "Atlanta", "description": "The ATL."],
+        ["name": "Boston", "description": "The City on a Hill."],
+        ["name": "Nashville", "description": "Music City, USA."],
+        ["name": "Portland", "description": "The City of Roses."],
+        ["name": "New Orleans", "description": "The Big Easy."],
+        ["name": "Las Vegas", "description": "Sin City."]
+    ]
+
     var body: some View {
-        Text("This is the Location history sheet")
+        NavigationView {
+            List {
+                ForEach(cities, id: \.self) { city in
+                    VStack(alignment: .leading, spacing: 8) {
+                        Text(city["name"] ?? "")
+                            .font(.title3)
+                            .bold()
+
+                        Text(city["description"] ?? "")
+                            .font(.body)
+                            .foregroundColor(.secondary)
+                    }
+                }
+            }
+            .navigationTitle("Location History")
+        }
     }
 }
 
